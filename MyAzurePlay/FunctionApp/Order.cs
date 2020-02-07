99768379caf819ebc0bde7b5f2c57fd0b68ed20a
Deyan Todorov <deyan.todorov@endava.com>
2020-02-07 11:10:35 +0200

    organize stuff and azure storage

---
+++ MyAzurePlay/FunctionApp/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace FunctionApp
+{
+    public class Order
+    {
+        public string PartitionKey { get; set; }
+
+        public string RowKey { get; set; }
+
+        public string Description { get; set; }
+    }
+}
