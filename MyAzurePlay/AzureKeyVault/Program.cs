31453004644be33695b98d8804d51c1c28261016
Deyan Todorov <deyan.todorov@endava.com>
2020-02-24 15:46:15 +0200

    Secure with Azure Key-Vault

---
+++ MyAzurePlay/AzureKeyVault/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+
+namespace AzureKeyVault
+{
+    public static class Program
+    {
+        private static async Task Main()
+        {
+            await RunAsync();
+        }
+
+        private static async Task RunAsync()
+        {
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+
+            var kvc = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+            var kvBaseUrl = "https://keyvault8627981.vault.azure.net/";
+
+            var secret = await kvc.GetSecretAsync(kvBaseUrl, "connectionString");
+
+            Console.WriteLine(secret);
+        }
+    }
+}
