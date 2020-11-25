8b269aea8bb6e60adc65d8337a2c021ab568d0ae
Deyan Todorov <deyan@MacBookPro.local>
2020-11-25 21:53:32 +0200

    c# example for Azure Relay

---
+++ csharp/AzureRelay/Listener/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Azure.Relay;
+
+namespace Listener
+{
+    public class Program
+    {
+        private const string RelayNamespace = "mydtrelay.servicebus.windows.net";
+        private const string ConnectionName = "checkconnection";
+        private const string KeyName = "RootManageSharedAccessKey";
+        private const string Key = "94rHzaDD3DxI1OYxB6O2mKF959pmrb0/01yQAOOIZGg=";
+
+        private static async Task Main()
+        {
+            Console.WriteLine("Starting status check listener.");
+            await RunAsync();
+        }
+
+        private static async Task RunAsync()
+        {
+            // Create the listener
+            var tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(KeyName, Key);
+            var listener = new HybridConnectionListener(new Uri($"sb://{RelayNamespace}/{ConnectionName}"), tokenProvider);
+
+            // Subscribe to the status events
+            listener.Connecting += (o, e) => { Console.WriteLine("Connecting"); };
+            listener.Offline += (o, e) => { Console.WriteLine("Check service is offline"); };
+            listener.Online += (o, e) => { Console.WriteLine("Check service is online"); };
+
+            // Create an array of credit status values
+            var statuses = new List<string>
+            {
+                "Good",
+                "Some issues",
+                "Bad"
+            };
+
+            // Provide an HTTP request handler
+            listener.RequestHandler = (context) =>
+            {
+                // Obtain the name from the request
+                var reader = new StreamReader(context.Request.InputStream);
+                var requestedName = reader.ReadToEnd();
+                Console.WriteLine("A request was received to check status for: " + requestedName);
+
+                // Pick a status at random
+                Random r = new Random();
+                var index = r.Next(statuses.Count);
+
+                // Formulate and send the response
+                context.Response.StatusCode = HttpStatusCode.OK;
+                context.Response.StatusDescription = "Status check successful";
+
+                using(var writer = new StreamWriter(context.Response.OutputStream))
+                {
+                    writer.WriteLine($"Status check for {requestedName}: {statuses[index]}");
+                }
+
+                // Close the context
+                context.Response.Close();
+            };
+
+            // Open the listener
+            await listener.OpenAsync();
+            Console.WriteLine("Server listening");
+
+            // Start a new thread that will continuously read the console.
+            await Console.In.ReadLineAsync();
+
+            // Close the listener after you exit the processing loop.
+            await listener.CloseAsync();
+        }
+    }
+}
---
+++ csharp/AzureRelay/Sender/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.Azure.Relay;
+
+namespace Sender
+{
+    public class Program
+    {
+        // Details of the Azure Relay
+        private const string RelayNamespace = "mydtrelay.servicebus.windows.net";
+        private const string ConnectionName = "checkconnection";
+        private const string KeyName = "RootManageSharedAccessKey";
+        private const string Key = "94rHzaDD3DxI1OYxB6O2mKF959pmrb0/01yQAOOIZGg=";
+
+        private static async Task Main()
+        {
+            Console.WriteLine("Starting credit check sender...");
+            await RunAsync();
+        }
+
+        private static async Task RunAsync()
+        {
+            // Get a name from the user
+            Console.WriteLine("Enter a name to check:");
+            var name = Console.ReadLine();
+
+            var tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(KeyName, Key);
+            var uri = new Uri($"https://{RelayNamespace}/{ConnectionName}");
+            var token = (await tokenProvider.GetTokenAsync(uri.AbsoluteUri, TimeSpan.FromHours(1))).TokenString;
+
+            // Create an HttpClient and formulate the request
+            var client = new HttpClient();
+            var request = new HttpRequestMessage()
+            {
+                RequestUri = uri,
+                Method = HttpMethod.Get,
+                Content = new StringContent(name)
+            };
+
+            request.Headers.Add("ServiceBusAuthorization", token);
+
+            // Send the request
+            var response = await client.SendAsync(request);
+
+            // Display the result
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+            // Wait for the user to press return
+            Console.ReadLine();
+        }
+    }
+}
